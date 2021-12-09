09e7916f04abf31d026d3d550d8c0c2148349cb1
Eduardo Granzoto Araujo <edugranzotoaraujo@gmail.com>
2021-12-09 11:48:40 -0300

    Add functions preload, complete, execute and headers

--- sweet-request.js
+++ sweet-request.js
@@ -1,31 +1,59 @@
-let sweet_request = (method, url, data, preload = '', complete = '', dataType = '') => {    
-    let info_data = {
-        status: '', 
-        msg: '', 
-        data: null
+let sweet_request = (method, url, data, dataType = '') => {            
+    let info_ajax = {
+        headers: '', 
+        preload: '', 
+        complete: ''
     }
 
-    $.ajax({
-        type: method,
-        url: url,
-        data: data,
-        dataType: dataType,   
-        async: false,
-        beforeSend: function() {
-            preload
-        },    
-        success: function (response) {
-            info_data.status = 'success',            
-            info_data.data = response
-        },
-        complete: function() {
+    let preload = (context) => {     
+        info_ajax.preload = context
+    }
 
-        },
-        error: function (request, status, error) {
-            info_data.status = status 
-            info_data.msg    = error
+    let complete = (context = '') => {
+        info_ajax.complete = context
+    }
+    
+    let headers = (object) => {
+        info_ajax.headers = object
+    }
+
+    let execute = async () => {
+        let info_data = {
+            status: '', 
+            msg: '', 
+            data: null
         }
-    });
 
-    return info_data
+        await $.ajax({
+            headers: info_ajax.headers,
+            type: method,
+            url: url,
+            data: data,
+            dataType: dataType,   
+            // async: false, 
+            beforeSend: function() {            
+                info_ajax.preload
+            },    
+            success: function (response) {     
+                info_data.status = 'success',            
+                info_data.data = response
+            },
+            complete: function() {
+                info_ajax.complete
+            },
+            error: function (request, status, error) {
+                info_data.status = status 
+                info_data.msg    = error
+            }
+        });
+        
+        return info_data
+    }  
+
+    return {
+        preload,
+        complete,
+        execute, 
+        headers        
+    }
 }
